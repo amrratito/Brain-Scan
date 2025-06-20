43f8ef0cfc8a3dd301a87a2c29c1150309bcab98
amrratito <amrfire03@gmail.com>
2025-06-20 19:18:52 +0300

    cloudinary

--- backend/utils/cloudinaryStorage.js
+++ backend/utils/cloudinaryStorage.js
@@ -1,6 +1,10 @@
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 const cloudinary = require('./cloudinary');
+const asyncHandler = require ('express-async-handler');
+const uuidv4  =require('uuid').v4;
+const sharp = require('sharp');
+const cloudinary = require ('../config/cloudinary.js');
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -11,6 +15,33 @@ const storage = new CloudinaryStorage({
   }
 });
 
+
+
 const upload = multer({ storage });
+
+
+
+const uploadToCloudinary = (buffer, filename, folder, format = 'jpeg', quality = 'auto') => {
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+            {
+                folder,
+                public_id: filename,
+                resource_type: 'image',
+                format,
+                quality,
+                overwrite: true
+            },
+            (error, result) => {
+                if (error) {
+                    reject(new Error(`Cloudinary Upload Error: ${error.message}`, 500));
+                } else {
+                    resolve(result);
+                }
+            }
+        );
+        stream.end(buffer);
+    });
+};
 
 module.exports = upload;
